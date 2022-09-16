58b4677824eb9f3a47f73ae2f568d5c0ac30fb38
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2022-09-16 12:27:33 +0200

    Update MapSchemaTest

--- src/test/java/hexlet/code/MapSchemaTest.java
+++ src/test/java/hexlet/code/MapSchemaTest.java
@@ -1,9 +1,9 @@
 package hexlet.code;
 
+import hexlet.code.schemas.BaseSchema;
 import hexlet.code.schemas.MapSchema;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,17 +11,17 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class MapSchemaTest {
 
+    private Validator v;
     private MapSchema schema;
 
     /**
      * Initializes a schema and a validator.
      */
-    @BeforeAll
+    @BeforeEach
     public void init() {
-        Validator v = new Validator();
+        v = new Validator();
         schema = v.map();
     }
 
@@ -44,5 +44,33 @@ public class MapSchemaTest {
         assertFalse(schema.isValid(data));
         data.put("key2", "value2");
         assertTrue(schema.isValid(data));
+    }
+
+    @Test
+    public void isValidWhenShape() {
+        Map<String, BaseSchema> schemas = new HashMap<>();
+        schemas.put("name", v.string().required());
+        schemas.put("age", v.number().positive());
+        schema.shape(schemas);
+
+        Map<String, Object> human1 = new HashMap<>();
+        human1.put("name", "Alex");
+        human1.put("age", 2);
+        assertTrue(schema.isValid(human1));
+
+        Map<String, Object> human2 = new HashMap<>();
+        human2.put("name", "Bob");
+        human2.put("age", null);
+        assertTrue(schema.isValid(human2));
+
+        Map<String, Object> human3 = new HashMap<>();
+        human3.put("name", "");
+        human3.put("age", null);
+        assertFalse(schema.isValid(human3));
+
+        Map<String, Object> human4 = new HashMap<>();
+        human4.put("name", "Jamie");
+        human4.put("age", -1);
+        assertFalse(schema.isValid(human4));
     }
 }
